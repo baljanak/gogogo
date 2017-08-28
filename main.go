9e4df8b7b865625d46884a5d7f972254f1ea972b
Balaji J <baljanak@gmail.com>
2017-08-28 19:23:45 +0530

    sample closures in go

--- main.go
+++ main.go
@@ -5,11 +5,30 @@ import (
 	"net/http"
 )
 
+func fibonacci() func() uint64 {
+	var n1, n2 uint64 = 1, 0
+	return func() uint64 {
+		defer func() {
+			n1 = n1 + n2
+			n2 = n1 - n2
+		}()
+		return n1 + n2
+	}
+}
+
+func testFib() {
+	f := fibonacci()
+	for i := 0; i < 50; i++ {
+		fmt.Println(f())
+	}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
+	testFib()
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
