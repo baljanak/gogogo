c7f413165147ebffb0b02eb932b204659706c91f
Balaji J <baljanak@gmail.com>
2017-09-07 08:28:01 +0530

    sample etcd watcher

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM scratch
 MAINTAINER Balaji J <baljanak@gmail.com>
 ADD build/gogogo gogogo
+ENV ETCD http://docker.for.mac.localhost:2379
+ENV HTTP_ADDR :8080
 EXPOSE 8080
 ENTRYPOINT ["/gogogo"]
--- main.go
+++ main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"time"
 
+	log "github.com/Sirupsen/logrus"
+	"github.com/coreos/etcd/client"
 	"github.com/julienschmidt/httprouter"
+	"golang.org/x/net/context"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -19,10 +24,46 @@ func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
+func WatchEtcd(e, cfile string) {
+
+	cfg := client.Config{
+		Endpoints:               []string{e},
+		Transport:               client.DefaultTransport,
+		HeaderTimeoutPerRequest: time.Second,
+	}
+	c, err := client.New(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	kapi := client.NewKeysAPI(c)
+
+	watcher := kapi.Watcher(cfile, nil)
+	for {
+		resp, err := watcher.Next(context.TODO())
+		if err != nil {
+			if _, ok := err.(*client.ClusterError); ok {
+				log.Error(err)
+				time.Sleep(time.Second * 5)
+				continue
+			}
+			log.Fatal(err)
+		}
+		log.Info("Updated value:\n%s", resp.Node.Value)
+		log.Info("go DoSomething()")
+	}
+}
+
 func main() {
+	l := os.Getenv("HTTP_ADDR")
+	e := os.Getenv("ETCD")
+	cfile := "/test/config/deployment.yaml"
+
+	go WatchEtcd(e, cfile)
+
 	router := httprouter.New()
 	router.GET("/", Index)
 	router.GET("/hello/:name", Hello)
 
-	http.ListenAndServe(":8080", router)
+	log.Info("Starting http server on ", l)
+	http.ListenAndServe(l, router)
 }
